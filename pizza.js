f5e28b2f9be3eeb6fe5b5cb25170bb4b786d69c0
ktran98 <peterkhanhtran@gmail.com>
2016-11-17 13:54:57 -0600

    Add files via upload
    the first thing

---
+++ pizza.js
@@ -0,0 +1,18 @@
+class pizza{
+  //type constructor below
+  constructor(size, meatToppings, veggieToppings){
+    this.size = s;
+    this.meatToppings = mT;
+    this.veggieToppings = vT;
+
+}
+  //type instance functions below
+sizeCost(){
+  
+}
+
+
+  //type class functions below
+
+
+}
