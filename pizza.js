1148137e882bb39bf191b8299c62749f75b5fe7f
ktran98 <peterkhanhtran@gmail.com>
2016-11-18 13:52:22 -0600

    Add files via upload
    finished

--- pizza.js
+++ pizza.js
@@ -7,12 +7,43 @@ class pizza{
 
 }
   //type instance functions below
-sizeCost(){
-  
-}
+sizeCost = function(){
+  this.size = s;
+  this.cost = c;
+  if this.size = "Small"
+  this.Cost = 7.99;
+  if this.size = "Medium"
+  this.Cost = 9.99;
+  if this.size = "Large"
+  this.Cost = 12.99;
+  if this.size = "ExtraLarge"
+  this.Cost = 15.99;
+  return this.Cost
+};
 
+toppingCost = function(top){
+  this.top = t;
+  if (this.top == "meat")
+  this.top = this.top + 0.99
+  return this.top
+
+  if (this.top == "veggie")
+  this.top = this.top + 0.50
+  return this.top
+};
+
+price = function(price){
+  this.price = this.Cost + this.top;
+  return this.price
+};
 
   //type class functions below
-
+promotionalDeal = function(pizza,percent){
+  this.pizza = p;
+  this.percent = d;
+  this.newCost = this.pizza*this.percent;
+  return this.newCost;
+  
+}
 
 }
